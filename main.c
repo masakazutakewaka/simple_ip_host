c51a9db692292c95be0614ee4464d39a0a059601
masakazutakewaka <takewakamma@gmail.com>
2019-01-15 01:03:50 +0900

    poll & read PF_PACKET

--- main.c
+++ main.c
@@ -24,3 +24,38 @@ int	EndFlag=0;
 int	DeviceSoc;
 
 PARAM	Param;
+
+void *MyEthThread(void *arg)
+{
+  int nready;
+  struct pollfd targets[1];
+  u_int8_t buf[2048];
+  int len;
+
+  targets[0].fd=DeviceSoc;
+  targets[0].events=POLLIN|POLLERR;
+
+  while(EndFlag==0){
+    switch((nready=poll(targets,1,1000))){
+      case -1:
+        if(erron!=EINTR){
+          perror("poll");
+        }
+        break;
+      case 0:
+        break;
+      default:
+        if(targets[0].rebents&(POLLIN|POLLERR)){
+          if((len=read(DeviceSoc,buf,sizeof(buf)))<=0){
+            perror("read");
+          }
+          else{
+            EtherRecv(DeviceSoc,buf,len);
+          }
+        }
+        break;
+    }
+  }
+
+  return(NULL);
+}
