7af97963aa76e991bd7e57d9a518056457a848ce
masakazutakewaka <takewakamma@gmail.com>
2019-01-19 00:58:12 +0900

    PARAM

--- param.h
+++ param.h
@@ -4,3 +4,14 @@
 
 #define DUMMY_WAIT_MS (100)
 #define RETRY_COUNT (3)
+
+typedef struct {
+  char *device;
+  struct in_addr myip;
+  u_int8_t vmac[6];
+  struct in_addr vip;
+  struct in_addr vmask;
+  int IpTTL;
+  int MTU;
+  struct in_addr gateway;
+}PARAMS;
